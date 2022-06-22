9d6fa07d6c1329241c23d9c63cd9c1d931bd7556
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-06-22 13:40:58 +0530

    Add files via upload

---
+++ AAAA.java
@@ -0,0 +1,18 @@
+class AAAA{
+public static void main(String[]args){
+
+int a = 20, b = 50;
+System.out.println(a);
+System.out.println(b);
+System.out.println("Sum of a + b : ");
+System.out.println(a+b);
+System.out.println("Sub of a - b : ");
+System.out.println(a-b);
+System.out.println("Mul of a * b : ");
+System.out.println(a*b);
+System.out.println("Div of a / b : ");
+System.out.println(a/b);
+
+
+}
+}
