770a37550fdce5a7c5dab3eedd5de29cf07bcee6
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-06-22 13:40:29 +0530

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,14 @@
+class FirstJava{
+public static void main(String[]args){
+//System.out.println("Printing 1 to 10");
+//for(int i=1; i<=10; i++){
+//System.out.println(i);
+//}
+//System.out.println("Sayan Das");
+//System.out.println("Course: Java Web Programming");
+//System.out.println("Stream: Electrical Engineering");
+int a = 20, b = 50;
+System.out.println(a+b);
+
+}
+}
