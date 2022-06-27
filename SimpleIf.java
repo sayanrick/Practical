1716374e1a413437db328bf701bfe59828ee722a
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-06-27 13:09:40 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,11 @@
+package com.java;
+
+public class SimpleIf {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        if (x+y < 10){
+            System.out.println("x + y is less than 10");
+        }
+    }
+}
