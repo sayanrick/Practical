47fd20eeda8ecdd933a03fd99901819aacb25266
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-06-28 10:38:27 +0530

    Add files via upload

---
+++ EligibleForVote.java
@@ -0,0 +1,16 @@
+package com.java;
+
+import java.util.Scanner;
+
+public class EligibleForVote {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your age:- ");
+        int age = in.nextInt();
+
+        if (age >= 18){
+            System.out.println("You are eligible for Vote.");
+        } else
+            System.out.println("You are not eligible Vote.");
+    }
+}
