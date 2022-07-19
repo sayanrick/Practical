2cdd5c6b19128f9a653fa6c3b9771affcd2ad6d0
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2022-07-19 17:40:14 +0530

    Add files via upload

---
+++ ClassNameArray.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class ClassNameArray {
+	public static void main(String[] args) {
+		int arr[] = {1, 2, 4};
+		Class  c = arr.getClass();
+		String name = c.getName();
+		System.out.println(c);
+		
+		
+	}
+
+}
